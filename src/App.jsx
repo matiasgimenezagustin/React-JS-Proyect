3453f099f504ae35342328806f53e7528d1c4d7a
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-05-05 23:57:21 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 
 import NavBar from './components/NavBar/NavBar';
-import Button from './components/Button/Button';
 import ItemListContainer from './Pages/ItemListContainer/ItemListContainer';
-import Input from './components/Input/Input';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import ItemDetailContainer from './Pages/ItemDetailContainer/ItemDetailContainer';
 
@@ -25,17 +23,6 @@ function App() {
         <Route path="/category/:categoryId" element={<ItemListContainer/>}/>
         <Route path="/item/:id" element={<ItemDetailContainer/>} />
       </Routes>
-
-
-      {/*<Button texto='Botton' color='red'/> */}
-
-{/*       <ItemListContainer greeting="Aun no se han publicado productos aqui." lista = {[1,2,3,4]}  /> */}
-
-  {/*  no usar () para no ejecutarla, solo usamos "extraerValor" como referencia de la funcion
-      <Input handleExtraer={extraerValor} id="1"/>
-      <Input handleExtraer={extraerValor} id="2"/> */}
-
-      
     </div>
   );
 }
--- src/Pages/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/Pages/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import ItemDetail from '../../components/ItemDetail/ItemDetail';
 
 
-function getItem() {
+function getItem(id) {
 
     const myPromise = new Promise((resolve, reject) => {
+        class Product{
+            constructor (id, image, title, descripcion, categoria, price, stock){
+                this.id = id;
+                this.image = image;
+                this.title = title;
+                this.descripcion = descripcion;
+                this.categoria = categoria;
+                this.price = price;
+                this.stock = stock;
+            }
+        }
 
-        /* const product1 = new Product(0, "https://josefacchin.com/wp-content/uploads/2016/02/landing-page-wordpress.png", "Landing Page", "Tu producto en solo una pagina", "Development", 15000, 3);
+        const product1 = new Product(0, "https://josefacchin.com/wp-content/uploads/2016/02/landing-page-wordpress.png", "Landing Page", "Tu producto en solo una pagina", "Development", 15000, 3);
 
-    const product2 = new Product(1, "https://marketing4ecommerce.net/wp-content/uploads/2020/11/nueva-portada-enero-1-6.jpg", "Web Desing", "Alcanza tus objetivos con una buena imagen", "Desing", 10000, 5); */
-        const item = {
-            id : 1,
-            title : "E-Commerce",
-            precio : "300$",
-            stock : 3,
-            url: "https://www.cronista.com/files/image/319/319739/60140d74c99af.jpg",
-            categoria : "Development",
-            descripcion : "Desarrolla tu negocio en el mundo digital",
-        };
+        const product2 = new Product(1, "https://marketing4ecommerce.net/wp-content/uploads/2020/11/nueva-portada-enero-1-6.jpg", "Web Desing", "Alcanza tus objetivos con una buena imagen", "Desing", 10000, 5);
+
+        const product3 = new Product(2, "https://www.cronista.com/files/image/319/319739/60140d74c99af.jpg", "E-Commerce", "Desarrolla tu negocio en el mundo digital", "Development", 70000, 4);
+
+        const data = [product1, product2, product3];
+
+    const item = data.filter( item => item.id == id);
+    console.log(item)
         setTimeout(() => {
-            resolve(item)
+            resolve(item[0])
         }, 2000)
     });
     return myPromise;
@@ -27,10 +37,11 @@ function getItem() {
 
 function ItemDetailContainer(props) {
 
-    
+    const {id} = useParams();
 
     const [item, setItem] = useState({});
     useEffect(() => {
+
         getItem()
             .then(res =>{
                 setItem(res);
@@ -46,5 +57,7 @@ function ItemDetailContainer(props) {
         </div>
     );
 }
+
+
 
 export default ItemDetailContainer;
