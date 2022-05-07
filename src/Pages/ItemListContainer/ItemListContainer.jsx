bfd8af7fd33a6904516824654aa1d40945ee84f5
lolazo455 <mati.a.gimenez2002@gmail.com>
2022-05-06 22:02:19 -0300

    bug fixed

--- src/Pages/ItemListContainer/ItemListContainer.jsx
+++ src/Pages/ItemListContainer/ItemListContainer.jsx
@@ -16,27 +16,21 @@ function getProducts (categoria) {
                 image: "https://josefacchin.com/wp-content/uploads/2016/02/landing-page-wordpress.png",
                 title:"Landing Page",
                 categoria: "development",
-                descripcion: "Tu producto en solo una pagina",
                 price : 15000,
-                stock : 3,
             },
             {
                 id: 1,
                 image: "https://marketing4ecommerce.net/wp-content/uploads/2020/11/nueva-portada-enero-1-6.jpg",
                 title:"Web Desing",
                 categoria: "desing",
-                descripcion: "Alcanza tus objetivos con una buena imagen",
                 price : 10000,
-                stock : 5,
             },
             {
                 id: 2,
                 image: "https://www.cronista.com/files/image/319/319739/60140d74c99af.jpg",
                 title: "E-Commerce",
                 categoria: "development",
-                descripcion: "Desarrolla tu negocio en el mundo digital",
                 price : 70000,
-                stock : 4,
             }
         ];
         const emptyFilter = () =>{
